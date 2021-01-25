96c5014db9d069bbf261ce2ba9ba4d3c912dae45
jamieberrier <berrier.jamie@gmail.com>
2021-01-25 13:24:19 -0800

    Update FrequencyCounterValidAnagram.js

---
+++ FrequencyCounterValidAnagram.js
@@ -0,0 +1,15 @@
+function validAnagram(s, t){
+  // check if string lengths are equal, if not, return false
+  
+  //  create object for each string
+  
+  // loop over 1st string and add to object
+  
+  // loop over 2nd string and add to object
+  
+  // loop over 2nd string's object and compare to 1st string's object
+    // check if key exists, return false if not
+    // check if values match, return false if not
+  
+  // if make it thru loop, return true
+}
