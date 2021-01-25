69627c941589a7756315773e158dd0a023d4624c
jamieberrier <berrier.jamie@gmail.com>
2021-01-25 13:58:49 -0800

    Update FrequencyCounter.js

--- FrequencyCounter.js
+++ FrequencyCounter.js
@@ -1,5 +1,6 @@
 // break down arrays/strings into objects and then compare the objects
 // use objects to construct profiles/break down contents of arrays or strings, and then quickly compare the breakdowns
+// e.g. compare for same pieces, numbers consist of same digits
 function same(arr1, arr2){
   // if arrays are not the same length, return false
   if(arr1.length !== arr2.length){
