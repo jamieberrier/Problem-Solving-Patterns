711e678f542f26c267b4aac3c9dc20ed1bca3ff8
jamieberrier <berrier.jamie@gmail.com>
2021-01-26 09:18:13 -0800

    Update MultiplePointers.js

--- MultiplePointers.js
+++ MultiplePointers.js
@@ -1,3 +1,7 @@
+// write a function called sumZero which accepys a sorted array of integers.
+// the function should find the 1st pair where the sum is 0.
+//return an array that includes both values that sum to 0 or undefined if a pair does not exist.
+
 function sumZero(arr){
   for(let i = 0; i < arr.length; i++){
     for(let j = i+1; j < arr.length; j++){
