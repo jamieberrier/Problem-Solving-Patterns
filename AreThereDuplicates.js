89dbb2d06e78e45551bdee11a15676f7b5c3ce51
jamieberrier <berrier.jamie@gmail.com>
2021-02-01 16:29:05 -0800

    Update AreThereDuplicates.js

--- AreThereDuplicates.js
+++ AreThereDuplicates.js
@@ -1,1 +1,9 @@
-//
+// Pattern: Frequency Counter / Multiple Pointers
+
+// Implement a function called areThereDuplicates which accepts a
+// variable number of arguments, and checks whether there are any
+// duplicates among the arguments passed in. 
+
+function areThereDuplicates() {
+  // good luck. (supply any arguments you deem necessary.)
+}
