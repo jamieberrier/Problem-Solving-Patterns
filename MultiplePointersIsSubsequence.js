29cf67fcfed21027af20e4ee0f29b2084776b69e
jamieberrier <berrier.jamie@gmail.com>
2021-02-02 10:54:47 -0800

    Update MultiplePointersIsSubsequence.js

--- MultiplePointersIsSubsequence.js
+++ MultiplePointersIsSubsequence.js
@@ -1,3 +1,14 @@
+// Write a function called isSubsequence which takes in two srings
+// and checks whether the characters in the first string for a subsequence
+// of the characters in the 2nd string. 
+// In other words, the function should check whether the characters in the
+// 1st string appear somewhere in the 2nd string, without their order changing
+
+// MUST have AT LEAST the following complexities:
+// Time: O(n + m)
+// Space: O(1)
+
+// 
 
 function isSubsequence() {
   // good luck. Add any arguments you deem necessary.
