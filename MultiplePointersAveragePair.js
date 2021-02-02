4eb33e8726ccaa606367d37bec6e5db2f36c6b39
jamieberrier <berrier.jamie@gmail.com>
2021-02-02 09:54:07 -0800

    Update MultiplePointersAveragePair.js

--- MultiplePointersAveragePair.js
+++ MultiplePointersAveragePair.js
@@ -10,6 +10,11 @@
 // Time: O(n)
 // Space: O(1)
 
+// averagePair([1,2,3], 2.5) // true
+// averagePair([1,3,3,5,6,7,10,12,19], 8) // true
+// averagePair([-1,0,3,4,5,6], 4.1) // false
+// averagePair([],4) // false
+
 function averagePair(){
   // add whatever parameters you deem necessary - good luck!
 }
