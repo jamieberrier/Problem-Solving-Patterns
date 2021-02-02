ccdaa89fd0b906670f1739dc3826bcd744a4b3d3
jamieberrier <berrier.jamie@gmail.com>
2021-02-01 16:58:39 -0800

    Update SameFrequency.js

--- SameFrequency.js
+++ SameFrequency.js
@@ -1,3 +1,7 @@
 // Pattern: Frequency Counter
 
-//
+// Write a function called sameFrequency
+// Given 2 positive integers, find out if the 2 numbers have the same frequency of digits
+
+// sameFrequency(182, 281) // true
+// sameFrequency(34, 14) // false
