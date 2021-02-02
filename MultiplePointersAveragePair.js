37d4bdcc17714f94b49d93e7c68773053d2ad72b
jamieberrier <berrier.jamie@gmail.com>
2021-02-02 09:52:12 -0800

    Update MultiplePointersAveragePair.js

--- MultiplePointersAveragePair.js
+++ MultiplePointersAveragePair.js
@@ -1,3 +1,15 @@
+// Pattern: Two Pointers
+
+// Write a function, averagePair
+// Given a sorted array of integers and a target average,
+// determine if there is a pair of values in the array
+// where the average of the pairs equals the target average.
+// There may be more than 1 pair that matches the average target
+
+// Bonus Constraints:
+// Time: O(n)
+// Space: O(1)
+
 function averagePair(){
   // add whatever parameters you deem necessary - good luck!
 }
