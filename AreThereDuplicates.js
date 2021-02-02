a521261f4219201146f7c4830cdb9591a391fed7
jamieberrier <berrier.jamie@gmail.com>
2021-02-01 16:55:17 -0800

    Update AreThereDuplicates.js

--- AreThereDuplicates.js
+++ AreThereDuplicates.js
@@ -4,6 +4,18 @@
 // variable number of arguments, and checks whether there are any
 // duplicates among the arguments passed in. 
 
-function areThereDuplicates() {
-  // good luck. (supply any arguments you deem necessary.)
+function areThereDuplicates(...args) {
+  let argsHash = {}
+
+  for(let element of args) {
+    // if element is in argsHash, add 1 to value
+    // if element not in argsHash, add it
+    argsHash[element] ? argsHash[element] += 1 : argsHash[element] = 1
+  }
+  // if any value is greater than 1, return true
+  for(let key in argsHash) {
+    if (argsHash[key] > 1) return true
+  }
+  // made it through loop, return false
+  return false
 }
