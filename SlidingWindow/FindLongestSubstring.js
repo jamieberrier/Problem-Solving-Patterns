cdea01fffe797a6fd51e55806edee028aca47756
jamieberrier <berrier.jamie@gmail.com>
2021-02-04 10:23:10 -0800

    Update FindLongestSubstring.js

--- SlidingWindow/FindLongestSubstring.js
+++ SlidingWindow/FindLongestSubstring.js
@@ -6,3 +6,11 @@
 // Time Complexity: O(n)
 
 // Examples:
+// findLongestSubstring('') // 0
+// findLongestSubstring('rithmschool') // 7
+// findLongestSubstring('thisisawesome') // 6
+// findLongestSubstring('thecatinthehat') // 7
+// findLongestSubstring('bbbbbb') // 1
+// findLongestSubstring('longestsubstring') // 8
+// findLongestSubstring('thisishowwedoit') // 6
+
