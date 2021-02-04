b39108344c5b7d267b37404a8984d866ba15c037
jamieberrier <berrier.jamie@gmail.com>
2021-02-04 09:48:53 -0800

    Update MinSubArrayLen.js

---
+++ SlidingWindow/MinSubArrayLen.js
@@ -0,0 +1,19 @@
+// Pattern: Sliding Window
+
+// Write a function called minSubArrayLen which accepts two parameters - 
+// an array of positive integers and a positive interger.
+// Should return the minimal length of a contiguous subarray of which
+// the sum >= the intger passed in. If there isn't one, return 0.
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// Examples:
+// minSubArrayLen([], )
+// minSubArrayLen([], )
+// minSubArrayLen([], )
+// minSubArrayLen([], )
+// minSubArrayLen([], )
+// minSubArrayLen([], )
+// minSubArrayLen([], )
+
