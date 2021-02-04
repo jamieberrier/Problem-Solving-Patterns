0a52e613bc3bc003a5ee6acf8f58b82b8b3284d9
jamieberrier <berrier.jamie@gmail.com>
2021-02-03 16:32:02 -0800

    Update SlidingWindowMaxSubarraySum.js

--- SlidingWindowMaxSubarraySum.js
+++ SlidingWindowMaxSubarraySum.js
@@ -1,13 +1,16 @@
 function maxSubarraySum(arr, subLength){
 	if (arr.length < subLength) return null
 
+	//maximum sum
 	let maxSum = 0
+	
   let tempSum = 0
 
+	// sum together the 1st subLength values of array
 	for (let i = 0; i < num; i++) {
     maxSum += arr[i];
 	}
-	
+	// set tempSum to maxSum
 	tempSum = maxSum
 
 	for (let i = num; i < arr.length; i++) {
