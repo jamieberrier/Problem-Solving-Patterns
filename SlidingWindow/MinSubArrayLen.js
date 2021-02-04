8a986439f7cc66c5c135ac79ff7a45c8bb5b935f
jamieberrier <berrier.jamie@gmail.com>
2021-02-04 09:52:48 -0800

    Update MinSubArrayLen.js

--- SlidingWindow/MinSubArrayLen.js
+++ SlidingWindow/MinSubArrayLen.js
@@ -9,11 +9,14 @@
 // Space Complexity: O(1)
 
 // Examples:
-// minSubArrayLen([], )
-// minSubArrayLen([], )
-// minSubArrayLen([], )
-// minSubArrayLen([], )
-// minSubArrayLen([], )
-// minSubArrayLen([], )
-// minSubArrayLen([], )
+// minSubArrayLen([2,3,1,2,4,3], 7) // 2 -> [4,3]
+// minSubArrayLen([2,1,6,5,4], 9) // 2 -> [5,4]
+// minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) // 1 -> [62]
+// minSubArrayLen([1,4,16,22,5,7,8,9,10], 39) // 3
+// minSubArrayLen([1,4,16,22,5,7,8,9,10], 55) // 5
+// minSubArrayLen([4,3,3,8,1,2,3], 11) // 2
+// minSubArrayLen([1,4,16,22,5,7,8,9,10], 95) // 0
 
+function minSubArrayLen(arr, num) {
+
+}
