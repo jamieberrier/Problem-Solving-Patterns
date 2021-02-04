f2e18b9094cddf232797b8d1d73053f862b05ef5
jamieberrier <berrier.jamie@gmail.com>
2021-02-04 10:20:57 -0800

    Update FindLongestSubstring.js

---
+++ SlidingWindow/FindLongestSubstring.js
@@ -0,0 +1,8 @@
+// Pattern: Sliding Window
+
+// Write a function called findLongestSubstring, which accepts a string
+// and returns the length of the longest substring with all distinct characters.
+
+// Time Complexity: O(n)
+
+// Examples:
