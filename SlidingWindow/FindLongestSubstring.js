ea60fe75e4288c3fc532bcd6e636163c8ef7a21d
jamieberrier <berrier.jamie@gmail.com>
2021-02-06 08:55:56 -0800

    Update FindLongestSubstring.js

--- SlidingWindow/FindLongestSubstring.js
+++ SlidingWindow/FindLongestSubstring.js
@@ -15,5 +15,33 @@
 // findLongestSubstring('thisishowwedoit') // 6
 
 function findLongestSubstring(str){
-  
+  if(str.length === 0 || str.length === 1) return str.length
+  // beginning of window
+  let start = 0
+  // end of window
+  let end = 1
+  // substring
+  let subStr = str[start]
+
+  // grow window (increase end) if next char doesn't match a char in substring
+  // if character matches, set start of window to end
+  while(start < end) {
+    // if substring doesn't include char at end
+    if(!subStr.includes(str[end])) {
+      // add char to subStr
+      subStr += str[end]
+      // increment end
+      ++end
+    } else if(subStr.length < str.substring(end).length){ // if length of remaining char > subStr.length
+      // set start of window to 1st char of dup
+      start = end
+      // set subStr to char at new start
+      subStr = str[start]
+      ++end
+    } else {
+      break
+    }
+  }
+
+  return subStr.length
 }
