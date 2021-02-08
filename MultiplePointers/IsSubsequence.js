cb2c0b4e96523efe180fc9560b61f7474f701531
jamieberrier <berrier.jamie@gmail.com>
2021-02-08 09:19:44 -0800

    Update IsSubsequence.js

--- MultiplePointers/IsSubsequence.js
+++ MultiplePointers/IsSubsequence.js
@@ -13,6 +13,17 @@
 // isSubsequence('abc', 'abracadabra') // true
 // isSubsequence('abc', 'acb') // false
 
-function isSubsequence(subStr, str) {
-  
+function isSubsequence(str1, str2) {
+    let i = 0;
+    let j = 0;
+
+    if (!str1) return true;
+
+    while (j < str2.length) {
+      if (str2[j] === str1[i]) i++;
+      if (i === str1.length) return true;
+      j++;
+    }
+    
+    return false;
 }
