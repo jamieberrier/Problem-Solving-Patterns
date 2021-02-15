55f8dba2676f576ceb3851b2c6dadb174f547f81
jamieberrier <berrier.jamie@gmail.com>
2021-02-15 09:42:36 -0800

    Update HelperMethodRecursion.js

--- FrequencyCounter/Recursion/HelperMethodRecursion.js
+++ FrequencyCounter/Recursion/HelperMethodRecursion.js
@@ -21,17 +21,18 @@ function collectOddValues(arr){
   let result = [];
 
   function helper(helperInput){
+    // if array length is 0, return
     if(helperInput.length === 0) {
       return;
     }
-    
+    // if value is odd, push onto result
     if(helperInput[0] % 2 !== 0){
       result.push(helperInput[0])
     }
-    
+    // call helper again w/ 1st element removed
     helper(helperInput.slice(1))
   }
-  
+  // 1st call to helper with original array
   helper(arr)
 
   return result;
