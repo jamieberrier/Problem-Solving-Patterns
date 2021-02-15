b0c7e21af83a1c1e9ff8728296164ef78eee0b85
jamieberrier <berrier.jamie@gmail.com>
2021-02-15 09:54:58 -0800

    Update PureRecursion.js

--- FrequencyCounter/Recursion/PureRecursion.js
+++ FrequencyCounter/Recursion/PureRecursion.js
@@ -18,4 +18,16 @@ function collectOddValues(arr){
 }
 
 collectOddValues([1,2,3,4,5])
-                           
+/*
+[1].concat(collectOddValues([2,3,4,5]))
+            [].concat(collectOddValues[3,4,5])
+                [3].concat(collectOddValues[4,5])
+                    [].concat(collectOddValues[5])
+                        [5].concat(collectOddValues[])
+--------------------------------------------------------------------------------------
+[1].concat([3,5]) // -> [1,3,5]
+            [].concat([3,5]) // -> [3,5]
+                        [3].concat([5]) // -> [3,5]
+                                    [].concat([5]) // -> [5]
+                                                [5].concat([]) // -> [5]
+*/
