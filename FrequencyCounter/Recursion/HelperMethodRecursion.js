87fe7c940fa904ed9d1f583669456903d1b95f42
jamieberrier <berrier.jamie@gmail.com>
2021-02-15 09:37:13 -0800

    Update HelperMethodRecursion.js

--- FrequencyCounter/Recursion/HelperMethodRecursion.js
+++ FrequencyCounter/Recursion/HelperMethodRecursion.js
@@ -1,3 +1,21 @@
+/* Helper Method Recursion Pattern
+  function outer(input) {
+    
+    var outerScopedVariable = []
+
+    function helper(helperInput) {
+      // modify the outerScopedVariable
+      helper(helperInput--)
+    }
+
+    helper(input)
+
+    return outerScopedVariable
+  }
+
+  outer(arr)
+*/
+
 function collectOddValues(arr){
     
   let result = [];
