c62f17a8d4e9a326ae40019205ae685bcc1c710b
jamieberrier <berrier.jamie@gmail.com>
2021-02-15 10:53:51 -0800

    Update ProductOfArray.js

---
+++ FrequencyCounter/Recursion/ProductOfArray.js
@@ -0,0 +1,6 @@
+// Wrte a function called productOfArray which takes in an array of numbers
+// and returns the product of them all
+
+// Examples:
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
