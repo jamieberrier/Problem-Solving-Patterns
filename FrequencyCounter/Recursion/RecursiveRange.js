281349b6f1f181590ec562e15ab93a215efbc0de
jamieberrier <berrier.jamie@gmail.com>
2021-02-15 11:02:22 -0800

    Update RecursiveRange.js

--- FrequencyCounter/Recursion/RecursiveRange.js
+++ FrequencyCounter/Recursion/RecursiveRange.js
@@ -5,6 +5,7 @@
 // recursiveRange(6) // 21
 // recursiveRange(10) // 55 
 
-function recursiveRange(){
-   
+function recursiveRange(num){
+  if(num === 0) return 0
+  return num + recursiveRange(--num)
 }
