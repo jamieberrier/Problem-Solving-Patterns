91deeae2ba74ea3e70a01d67ed250d9c143e4382
jamieberrier <berrier.jamie@gmail.com>
2021-02-15 09:25:57 -0800

    Update Factorial.js

--- FrequencyCounter/Recursion/Factorial.js
+++ FrequencyCounter/Recursion/Factorial.js
@@ -1,3 +1,4 @@
+// iteratively
 function factorial(num){
   let total = 1;
   for(let i = num; i > 1; i--){
@@ -6,7 +7,7 @@ function factorial(num){
   return total;
 }
 
-
+// recursively
 function factorial(num){
   if(num === 1) return 1;
   return num * factorial(num-1);
