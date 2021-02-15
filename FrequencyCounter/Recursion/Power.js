a5145cff5ea68994dd8cc600ac7a59c560f67c40
jamieberrier <berrier.jamie@gmail.com>
2021-02-15 10:29:12 -0800

    Update Power.js

--- FrequencyCounter/Recursion/Power.js
+++ FrequencyCounter/Recursion/Power.js
@@ -1,4 +1,9 @@
+// Write a function called power which accepts a base and an exponent.
+// The function should return the power of the base to the exponent.
+// This function should mimic the functionality of Math.pow()
+// Don't worry about negative bases and exponents
 
+// Examples:
 // power(2,0) // 1
 // power(2,2) // 4
 // power(2,4) // 16
