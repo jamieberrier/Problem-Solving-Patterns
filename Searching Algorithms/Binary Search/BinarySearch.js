5951b777588d20c59d27b888e4860019b76227a4
jamieberrier <berrier.jamie@gmail.com>
2021-02-23 12:46:37 -0800

    Update BinarySearch.js

---
+++ Searching Algorithms/Binary Search/BinarySearch.js
@@ -0,0 +1,9 @@
+// accepts a SORTED array and a value
+// returns the index of that value in the array
+// if value is not in the array, returns -1
+
+// binarySearch([1,2,3,4,5],2) -> 1
+
+function binarySearch(arr, num){
+  
+}
