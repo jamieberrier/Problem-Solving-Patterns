859bb1003bd9ea5535d9c2a104ff6235a0187c40
jamieberrier <berrier.jamie@gmail.com>
2021-02-23 12:59:09 -0800

    Update BinarySearch.js

--- Searching Algorithms/Binary Search/BinarySearch.js
+++ Searching Algorithms/Binary Search/BinarySearch.js
@@ -5,5 +5,20 @@
 // binarySearch([1,2,3,4,5],2) -> 1
 
 function binarySearch(arr, num){
-  
+  let left = 0
+  let right = arr.length -1
+  let mid
+
+  while(left <= right) {
+    // create middle index
+    mid = Math.floor((left + right) / 2)
+    // if value matches, return index
+    if(arr[mid] === num) return mid
+    // if value too large, move right pointer
+    if(arr[mid] > num) right = mid - 1
+    // if value too small, move left pointer
+    if(arr[mid] < num) left = mid + 1
+  }
+
+  return -1
 }
