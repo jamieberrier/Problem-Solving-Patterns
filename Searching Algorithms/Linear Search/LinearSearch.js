1e202872bf824c4592093b65106079800b655139
jamieberrier <berrier.jamie@gmail.com>
2021-02-23 12:23:33 -0800

    Update LinearSearch.js

--- Searching Algorithms/Linear Search/LinearSearch.js
+++ Searching Algorithms/Linear Search/LinearSearch.js
@@ -1,3 +1,15 @@
-function linearSearch(){
-  
+// Write a function that accepts an array and a value
+// and returns the index of the value. If the value
+// does not exist, return -1
+
+// linearSearch([10,15,20,25,30],15) -> 1
+
+function linearSearch(arr, num){
+  let numIndex = -1
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === num) numIndex = i
+  }
+
+  return numIndex
 }
