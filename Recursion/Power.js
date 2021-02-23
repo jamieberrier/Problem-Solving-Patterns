bd80b13d2800d1340122b3cb2b5b4c35418c6ae7
jamieberrier <berrier.jamie@gmail.com>
2021-02-23 12:13:58 -0800

    Create Power.js

---
+++ Recursion/Power.js
@@ -0,0 +1,16 @@
+// Write a function called power which accepts a base and an exponent.
+// The function should return the power of the base to the exponent.
+// This function should mimic the functionality of Math.pow()
+// Don't worry about negative bases and exponents
+
+// Examples:
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(base, exponent){
+  // base case
+  if(exponent === 0) return 1
+
+  return base * power(base, --exponent)
+}
