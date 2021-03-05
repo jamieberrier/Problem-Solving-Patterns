07440ea1d8fa3df21005a65ffba6ed7d5b923aa3
jamieberrier <berrier.jamie@gmail.com>
2021-03-05 10:38:12 -0800

    Update template2.js

--- Searching Algorithms/Binary Search/Technique 1/Template II/template2.js
+++ Searching Algorithms/Binary Search/Technique 1/Template II/template2.js
@@ -1,26 +1,3 @@
-/*
-
-int binarySearch(int[] nums, int target){
-  if(nums == null || nums.length == 0)
-    return -1;
-
-  int left = 0, right = nums.length;
-  while(left < right){
-    // Prevent (left + right) overflow
-    int mid = left + (right - left) / 2;
-    if(nums[mid] == target){ return mid; }
-    else if(nums[mid] < target) { left = mid + 1; }
-    else { right = mid; }
-  }
-
-  // Post-processing:
-  // End Condition: left == right
-  if(left != nums.length && nums[left] == target) return left;
-  return -1;
-}
-
-*/
-
 function binarySearch(nums, target){
   if(nums === null || nums.length === 0) return -1
 
@@ -41,6 +18,6 @@ function binarySearch(nums, target){
 
   // Post-processing
   // End Condition: left === right
-  if(left !== nums.length && nums[left] === target) return left;
-  return -1;
+  if(left !== nums.length && nums[left] === target) return left
+  return -1
 }
