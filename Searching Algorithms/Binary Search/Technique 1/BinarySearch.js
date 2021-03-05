4d479833e69250941d058369b700eb0b294a5f72
jamieberrier <berrier.jamie@gmail.com>
2021-03-05 09:54:05 -0800

    Update BinarySearch.js

--- Searching Algorithms/Binary Search/Technique 1/BinarySearch.js
+++ Searching Algorithms/Binary Search/Technique 1/BinarySearch.js
@@ -5,16 +5,18 @@
 // binarySearch([1,2,3,4,5],2) -> 1
 
 var search = function(nums, target) {
-  let start = 0,end = nums.length-1
+  let start = 0
+  let end = nums.length-1
+
   while(start <= end){
-      let mid = Math.floor((start+end)/2)
-      if(nums[mid] == target){
-          return mid
-      }else if(nums[mid] < target){
-          start = mid+1
-      }else{
-          end = mid-1
-      }
+    let mid = Math.floor((start+end)/2)
+    if(nums[mid] == target){
+      return mid
+    }else if(nums[mid] < target){
+      start = mid+1
+    }else{
+      end = mid-1
+    }
   }
   return -1
 };
