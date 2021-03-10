7482fb775dd46e78d13fe7169e60f407cc20e28f
jamieberrier <berrier.jamie@gmail.com>
2021-03-10 15:57:45 -0800

    Update 658.FindKClosestElements.js

--- Searching Algorithms/Binary Search/Template III/658.FindKClosestElements.js
+++ Searching Algorithms/Binary Search/Template III/658.FindKClosestElements.js
@@ -30,5 +30,17 @@ Constraints:
  * @return {number[]}
  */
  var findClosestElements = function(arr, k, x) {
-    
+  let left = 0
+  let right = arr.length - 1
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2)
+
+    if(x - arr[mid] > arr[mid + k] - x) {
+      left = mid + 1
+    } else {
+      right = mid
+    }
+  }
+  return arr.slice(left, left + k)
 };
