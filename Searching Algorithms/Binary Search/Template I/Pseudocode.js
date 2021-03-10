10678c2623a7d2957469581075a5327ffd7022c4
jamieberrier <berrier.jamie@gmail.com>
2021-03-10 12:36:42 -0800

    Create Pseudocode.js

---
+++ Searching Algorithms/Binary Search/Template I/Pseudocode.js
@@ -0,0 +1,8 @@
+// create a left pointer - at the start of the array
+// create a right pointer - at the end of the array
+// while left pointer comes before right pointer
+// // create a middle pointer
+// // if value found, return the index
+// // if value is too small, move left pointer up
+// // if value is too large, move right pointer down
+// if value not found, return -1
