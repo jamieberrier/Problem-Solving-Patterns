861f591fd01cd9f0806f3a1f81da14e14ead8d2c
jamieberrier <berrier.jamie@gmail.com>
2021-03-10 12:46:44 -0800

    Update template3.js

--- Searching Algorithms/Binary Search/Template III/template3.js
+++ Searching Algorithms/Binary Search/Template III/template3.js
@@ -5,7 +5,7 @@ int binarySearch(int[] nums, int target) {
     return -1;
 
   int left = 0, right = nums.length - 1;
-  
+
   while (left + 1 < right){
     // Prevent (left + right) overflow
     int mid = left + (right - left) / 2;
@@ -28,5 +28,28 @@ int binarySearch(int[] nums, int target) {
 */
 
 function binarySearch(nums, target){
-  if (nums === null || nums.length === 0) return -1;
+  if(nums === null || nums.length === 0) return -1
+
+  let left = 0
+  let right = nums.length - 1
+
+  while(left + 1 < right) {
+    let mid = Math.floor((left + right) / 2)
+
+    if(nums[mid] === target) {
+      return mid
+    } else if(nums[mid] < target) {
+      // search right
+      left = mid
+    } else {
+      // search left
+      right = mid
+    }
+  }
+
+  // Post-processing:
+  // End Condition: left + 1 === right
+  if(nums[left] === target) return left
+  if(nums[right] === target) return right
+  return -1
 }
