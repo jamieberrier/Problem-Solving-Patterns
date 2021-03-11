4dac0b54871c23d622c31c3d1cac908404047984
jamieberrier <berrier.jamie@gmail.com>
2021-03-10 17:08:30 -0800

    Update 658.FindKClosestElements.js

--- Searching Algorithms/Binary Search/Template III/658.FindKClosestElements.js
+++ Searching Algorithms/Binary Search/Template III/658.FindKClosestElements.js
@@ -1,5 +1,6 @@
 /*
-Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. 
+Given a sorted integer array arr, two integers k and x, 
+return the k closest integers to x in the array. 
 The result should also be sorted in ascending order.
 
 An integer a is closer to x than an integer b if:
@@ -44,3 +45,35 @@ Constraints:
   }
   return arr.slice(left, left + k)
 };
+
+/*
+
+Approach 2: Binary Search and Two Pointers
+Algorithm
+
+The original array has been sorted so we can take this advantage by the following steps.
+
+1. If the target x is less or equal than the first element in the sorted array, 
+  the first k elements are the result.
+2. Similarly, if the target x is more or equal than the last element in the sorted array, 
+  the last k elements are the result.
+3. Otherwise, we can use binary search to find the index of the element, which is equal (when this list has x) 
+  or a little bit larger than x (when this list does not have it). 
+  Then set low to its left k-1 position, and high to the right k-1 position of this index as a start. 
+  The desired k numbers must in this rang [index-k-1, index+k-1]. 
+
+  So we can shrink this range to get the result using the following rules.
+  * If low reaches the lowest index 0 or the low element is closer to x than the high element, 
+    decrease the high index.
+  * If high reaches to the highest index arr.size()-1 or it is nearer to x than the low element, 
+    increase the low index.
+  * The looping ends when there are exactly k elements in [low, high], 
+    the subList of which is the result.
+
+*/
+
+var findClosestElements = function(arr, k, x) {
+  if(x <= arr[0]) return arr.slice(0, k)
+
+  if(x >= arr[arr.length - 1]) return arr.slice(-k)
+}
