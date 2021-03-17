d4e3090049846d4cc25dd5873ab28a7f4452fae0
jamieberrier <berrier.jamie@gmail.com>
2021-03-17 09:44:40 -0700

    Update naiveStringSearch.js

--- Searching Algorithms/Naive String Search/naiveStringSearch.js
+++ Searching Algorithms/Naive String Search/naiveStringSearch.js
@@ -1,27 +1,28 @@
 /*
 Pseudocode
-- loop over the longer string
-- loop over the shorter string
-- if the characters don't match, break out of the inner loop
-- if the characters do match keep going
-- if you complete the inner loop and find a match, increment the count of matches
-- return the count
+1. loop over the longer string
+2. loop over the shorter string
+3. if the characters don't match, break out of the inner loop
+4. if the characters do match keep going
+5. if you complete the inner loop and find a match, increment the count of matches
+6. return the count
 */
 
 function stringSearch(long, short) {
   let count = 0
 
-  // outer loop
+  // 1. loop over the longer string
   for(let i = 0; i < long.length; i++) {
-    // inner loop
+    // 2. loop over the shorter string
     for(let j = 0; j < short.length; j++) {
+      // 3. if the characters don't match, break out of the inner loop
       if(long[i] !== short[j]) break
-
-      if(j === short.length - 1 ) ++count
-
+      // 4. if the characters do match keep going
       ++i
+      // 5. if you complete the inner loop and find a match, increment the count of matches
+      if(j === short.length - 1 ) ++count
     }
   }
-
+  // 6. return the count
   return count
 }
