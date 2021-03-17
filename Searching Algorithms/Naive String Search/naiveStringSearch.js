99c12a54149751eccd38f649f5afd653f53985b8
jamieberrier <berrier.jamie@gmail.com>
2021-03-17 09:06:02 -0700

    Update naiveStringSearch.js

---
+++ Searching Algorithms/Naive String Search/naiveStringSearch.js
@@ -0,0 +1,9 @@
+/*
+Pseudocode
+- loop over the longer string
+- loop over the shorter string
+- if the characters don't match, break out of the inner loop
+- if the characters do match keep going
+- if you complete the inner loop and find a match, increment the count of matches
+- return the count
+*/
