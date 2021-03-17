2e7310397f8795c13af7651c5a7433ae4f4cf3b0
jamieberrier <berrier.jamie@gmail.com>
2021-03-17 12:01:58 -0700

    Update bubbleSort.js

--- Searching Algorithms/Bubble Sort/bubbleSort.js
+++ Searching Algorithms/Bubble Sort/bubbleSort.js
@@ -1,5 +1,5 @@
 /*
-Pseudocode
+Pseudocode: array of numbers
   1. Start looping, with a variable called i, from the end to the beginning of array 
   2. Start an inner loop, with a variable called j, from the beginning until i - 1
   3. if array[j] > array[j + 1], swap the 2 values
@@ -11,3 +11,7 @@ Pseudocode
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
   }
 */
+
+function bubbleSort(arr) {
+
+}
