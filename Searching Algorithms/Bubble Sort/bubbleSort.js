0bc98b6c85adccc188aa48cb3ba1c93c18c30370
jamieberrier <berrier.jamie@gmail.com>
2021-03-17 13:26:35 -0700

    Update bubbleSort.js

--- Searching Algorithms/Bubble Sort/bubbleSort.js
+++ Searching Algorithms/Bubble Sort/bubbleSort.js
@@ -12,6 +12,17 @@ Pseudocode: array of numbers
   }
 */
 
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+}
+
 function bubbleSort(arr) {
 
+  for(let i = arr.length - 1; i > 0; i--) {
+    for(let j = 0; j <= i - 1; j++) {
+      // if(arr[j] > arr[j + 1]) swap(arr, j, j + 1)
+      if(arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+    }
+  }
+  return arr
 }
