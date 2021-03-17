6da9e61202d30c7431d5a40c7e7036394ab89e8b
jamieberrier <berrier.jamie@gmail.com>
2021-03-17 09:48:26 -0700

    Update naiveStringSearch.js

--- Searching Algorithms/Naive String Search/naiveStringSearch.js
+++ Searching Algorithms/Naive String Search/naiveStringSearch.js
@@ -26,3 +26,18 @@ function stringSearch(long, short) {
   // 6. return the count
   return count
 }
+
+/* Udemy
+function naiveSearch(long, short){
+    var count = 0;
+    for(var i = 0; i < long.length; i++){
+        for(var j = 0; j < short.length; j++){
+           if(short[j] !== long[i+j]) break;
+           if(j === short.length - 1) count++;
+        }
+    }
+    return count;
+}
+
+naiveSearch("lorie loled", "lol")
+*/
