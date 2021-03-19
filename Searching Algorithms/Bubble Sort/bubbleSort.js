a751f387837fa9799c8324a466dd2bdc7bb4f6ed
jamieberrier <berrier.jamie@gmail.com>
2021-03-19 12:39:52 -0700

    Update bubbleSort.js

--- Searching Algorithms/Bubble Sort/bubbleSort.js
+++ Searching Algorithms/Bubble Sort/bubbleSort.js
@@ -32,14 +32,14 @@ function bubbleSort(arr) {
 
 // Optimized BubbleSort with noSwaps
 function bubbleSort(arr){
-  var noSwaps;
-  for(var i = arr.length; i > 0; i--){
+  let noSwaps;
+  for(let i = arr.length; i > 0; i--){
     noSwaps = true;
-    for(var j = 0; j < i - 1; j++){
+    for(let j = 0; j < i - 1; j++){
       if(arr[j] > arr[j+1]){
-        var temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[ j+ 1] = temp
         noSwaps = false;         
       }
     }
