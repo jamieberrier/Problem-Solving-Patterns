a3f41c8c6f5f6791c27b3686534b33b71f84ba7d
jamieberrier <berrier.jamie@gmail.com>
2021-03-24 10:27:05 -0700

    Update selectionSort.js

--- Sorting Algorithms/Selection Sort/selectionSort.js
+++ Sorting Algorithms/Selection Sort/selectionSort.js
@@ -5,5 +5,8 @@ Pseudocode:
 - if a smaller number is found, designate that smaller value to be the new min - continue until the end of the array
 - if the min is not the value (index) you initially began with, swap the 2 values
 - repeat this with the next item until the array is sorted - shrinking the window
-
 */
+
+function selectionSort(arr) {
+
+}
