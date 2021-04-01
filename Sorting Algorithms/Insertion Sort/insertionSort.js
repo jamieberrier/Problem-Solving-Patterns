772e9328b053f14a0e3928fde3b8c68f92d22606
jamieberrier <berrier.jamie@gmail.com>
2021-04-01 08:08:34 -0700

    Update insertionSort.js

--- Sorting Algorithms/Insertion Sort/insertionSort.js
+++ Sorting Algorithms/Insertion Sort/insertionSort.js
@@ -11,12 +11,12 @@
 
 
 function insertionSort(arr){
-	var currentVal;
+	let currentVal;
 
-  for(var i = 1; i < arr.length; i++){
+  for(let i = 1; i < arr.length; i++){
     currentVal = arr[i];
 
-    for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j]
     }
     arr[j + 1] = currentVal;
